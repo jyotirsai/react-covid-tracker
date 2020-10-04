cce66d5f066ed5f991bc7db555b49dd7ee4ab452
jyotirsai <jyotirsai28@gmail.com>
2020-10-04 00:31:08 -0600

    first commit

---
+++ src/components/Plots.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Plots = () => {
+  return (
+    <div>
+      <h2>Plots</h2>
+    </div>
+  );
+};
+
+export default Plots;
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <h2>Search Functionality</h2>
+    </div>
+  );
+};
+
+export default Search;

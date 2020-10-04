c916ea05fc5c5aa25cdf0b740a05dcfe6bb56a6b
jyotirsai <jyotirsai28@gmail.com>
2020-10-04 14:34:21 -0600

    data passed to summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Typography, Grid, Paper } from "@material-ui/core";
 
-const Summary = () => {
+const Summary = (props) => {
   return (
     <div>
-      <h2>Summary</h2>
+      <Typography variant="h4">Summary</Typography>
+      <Grid container spacing={3}>
+        <Grid item>
+          <Paper>
+            <Typography variant="h6">Worldwide</Typography>
+          </Paper>
+        </Grid>
+        <Grid item>
+          <Paper>
+            <Typography variant="h6">
+              Country: {props.selectedCountry}
+            </Typography>
+            {typeof props.summary.Countries !== "undefined"
+              ? props.summary.Countries[0].TotalConfirmed
+              : null}
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };

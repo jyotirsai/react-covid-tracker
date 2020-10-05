3a2efdebf7903328ed30bb8a34a874e96c4efa4e
jyotirsai <jyotirsai28@gmail.com>
2020-10-04 20:59:11 -0600

    plot added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [summary, setSummary] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState("Canada");
+  const [confirmedData, setConfirmedData] = useState([]);
 
   // fetch data
   useEffect(() => {
@@ -16,10 +17,17 @@ const App = () => {
     axios.get("https://api.covid19api.com/countries").then((res) => {
       setCountries(res.data);
     });
-    // fetch covid data
+    // fetch covid summary data
     axios.get("https://api.covid19api.com/summary").then((res) => {
       setSummary(res.data);
     });
+
+    // fetch plot data
+    axios
+      .get("https://api.covid19api.com/dayone/country/canada/status/confirmed")
+      .then((res) => {
+        setConfirmedData(res.data);
+      });
   }, []);
 
   return (
@@ -27,7 +35,7 @@ const App = () => {
       <Typography variant="h1">COVID-19 Tracker</Typography>
       <Search countries={countries} setSelectedCountry={setSelectedCountry} />
       <Summary selectedCountry={selectedCountry} summary={summary} />
-      <Plots />
+      <Plots confirmedData={confirmedData} />
     </div>
   );
 };
--- src/components/Plots.js
+++ src/components/Plots.js
@@ -1,9 +1,33 @@
 import React from "react";
+import {
+  PieChart,
+  Pie,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 
-const Plots = () => {
+const Plots = (props) => {
+  let array = [];
+
+  Object.entries(props.confirmedData).forEach((entry) => {
+    let [key, value] = entry;
+    let obj = { Cases: value["Cases"], Date: key };
+    array.push(obj);
+  });
+
+  console.log(array);
+
   return (
     <div>
-      <h2>Plots</h2>
+      <AreaChart width={700} height={400} data={array}>
+        <XAxis dataKey="Date" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="Cases" fill="#8884d8" />
+      </AreaChart>
     </div>
   );
 };

205e1e4c50efeaaac0626f06651c28c312eef310
jyotirsai <jyotirsai28@gmail.com>
2020-10-05 21:33:34 -0600

    pie chart global

--- src/components/Plots.js
+++ src/components/Plots.js
@@ -4,6 +4,25 @@ import { PieChart, Pie, Tooltip } from "recharts";
 
 const Plots = (props) => {
   console.log(props.summary);
+  let data01 = [{}];
+  let globalTotalConfirmed = "";
+  let globalTotalRecovered = "";
+  let globalTotalDeaths = "";
+
+  if (typeof props.summary.Global !== "undefined") {
+    globalTotalConfirmed = props.summary.Global.TotalConfirmed;
+    globalTotalDeaths = props.summary.Global.TotalDeaths;
+    globalTotalRecovered = props.summary.Global.TotalRecovered;
+
+    data01 = [
+      { name: "TotalConfirmed:", value: globalTotalConfirmed },
+      { name: "TotalRecovered:", value: globalTotalRecovered },
+      { name: "TotalDeaths:", value: globalTotalDeaths },
+    ];
+  } else {
+    console.log("loading");
+  }
+
   return (
     <div>
       <PieChart width={800} height={400}>
@@ -13,6 +32,15 @@ const Plots = (props) => {
           cy={200}
           outerRadius={80}
           fill="#8884d8"
+          data={data01}
+        />
+        <Pie
+          dataKey="name"
+          cx={200}
+          cy={200}
+          outerRadius={80}
+          fill="#8884d8"
+          data={data01}
           label
         />
         <Tooltip />
